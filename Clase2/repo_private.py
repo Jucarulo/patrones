3b826cfc63f3d71ecb4233abf4f21b686c845b60
Jucarulo <106050050+Jucarulo@users.noreply.github.com>
2022-08-29 13:51:50 -0500

    Update repo_private.py

--- Clase2/repo_private.py
+++ Clase2/repo_private.py
@@ -4,7 +4,7 @@ import os
 print(os.path.dirname(os.path.realpath(__file__)))
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
-GIT_KEY="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"
+GIT_KEY=""
 
 with open(this_dir + "/repo_keys.txt") as f:
 	for line in f:
